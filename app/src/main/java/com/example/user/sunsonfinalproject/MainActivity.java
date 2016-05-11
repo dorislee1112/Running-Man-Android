315494bb36a7f01533ed6695144a2541678589de
dorislee1112 <dorislee1112@gmail.com>
2016-05-11 22:03:34 +0800

    create

---
+++ app/src/main/java/com/example/user/sunsonfinalproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.sunsonfinalproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
