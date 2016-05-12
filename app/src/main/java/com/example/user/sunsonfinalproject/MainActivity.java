3644c68a5d93b2fd2fc05d8871eb86aa39e0778e
stacey84318 <stacey84318@yahoo.com.tw>
2016-05-12 10:51:01 +0800

    Shake
    Stacey

--- app/src/main/java/com/example/user/sunsonfinalproject/MainActivity.java
+++ app/src/main/java/com/example/user/sunsonfinalproject/MainActivity.java
@@ -8,12 +8,94 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 public class MainActivity extends AppCompatActivity {
+    private SensorManager mSensorManager;   //體感(Sensor)使用管理
+    private Sensor mSensor;                 //體感(Sensor)類別
+    private float mLastX;                    //x軸體感(Sensor)偏移
+    private float mLastY;                    //y軸體感(Sensor)偏移
+    private float mLastZ;                    //z軸體感(Sensor)偏移
+    private double mSpeed;                 //甩動力道數度
+    private long mLastUpdateTime;           //觸發時間
+    int num = 0;
+    //甩動力道數度設定值 (數值越大需甩動越大力，數值越小輕輕甩動即會觸發)
+    private static final int SPEED_SHRESHOLD = 3000;
 
+    //觸發間隔時間
+    private static final int UPTATE_INTERVAL_TIME = 70;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //取得體感(Sensor)服務使用權限
+        mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+
+        //取得手機Sensor狀態設定
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //註冊體感(Sensor)甩動觸發Listener
+        mSensorManager.registerListener(SensorListener, mSensor, SensorManager.SENSOR_DELAY_GAME);
+
     }
+    private SensorEventListener SensorListener = new SensorEventListener()
+    {
+
+        public void onSensorChanged(SensorEvent mSensorEvent)
+        {
+            //當前觸發時間
+            long mCurrentUpdateTime = System.currentTimeMillis();
+
+            //觸發間隔時間 = 當前觸發時間 - 上次觸發時間
+            long mTimeInterval = mCurrentUpdateTime - mLastUpdateTime;
+
+            //若觸發間隔時間< 70 則return;
+            if (mTimeInterval < UPTATE_INTERVAL_TIME) return;
+
+            mLastUpdateTime = mCurrentUpdateTime;
+
+            //取得xyz體感(Sensor)偏移
+            float x = mSensorEvent.values[0];
+            float y = mSensorEvent.values[1];
+            float z = mSensorEvent.values[2];
+
+            //甩動偏移速度 = xyz體感(Sensor)偏移 - 上次xyz體感(Sensor)偏移
+            float mDeltaX = x - mLastX;
+            float mDeltaY = y - mLastY;
+            float mDeltaZ = z - mLastZ;
+
+            mLastX = x;
+            mLastY = y;
+            mLastZ = z;
+
+            //體感(Sensor)甩動力道速度公式
+            mSpeed = Math.sqrt(mDeltaX * mDeltaX + mDeltaY * mDeltaY + mDeltaZ * mDeltaZ)/ mTimeInterval * 10000;
+            //若體感(Sensor)甩動速度大於等於甩動設定值則進入 (達到甩動力道及速度)
+            if (mSpeed >= SPEED_SHRESHOLD)
+            {
+                //達到搖一搖甩動後要做的事情
+                Log.d("TAG","搖一搖中...");
+
+
+            }
+        }
+
+        public void onAccuracyChanged(Sensor sensor , int accuracy)
+        {
+        }
+    };
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        //在程式關閉時移除體感(Sensor)觸發
+        mSensorManager.unregisterListener(SensorListener);
+    }
+
 }
